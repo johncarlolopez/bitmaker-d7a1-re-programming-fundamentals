2eca9412bfdc9e368e59b81a42b30f2c3da7877b
John Carlo Lopez <condottiere.jc@gmail.com>
2017-12-12 14:19:28 -0500

    complete stretch for exercise 2

--- exercise2.rb
+++ exercise2.rb
@@ -17,13 +17,23 @@ if doc_ans >= 4
   puts "Since you like documentaries, I recommend  #{documentary_rec}"
 end
 if  (doc_ans <= 3)
-      if (dram_ans >= 4) && (com_ans >= 4)
-        puts "Since you like drama and comedy movies, I recommend  #{dramedy_rec}"
-      elsif (dram_ans >= 4)
-        puts "Since you like drama movies, I recommend  #{drama_rec}"
-      elsif (com_ans >= 4)
-        puts "Since you like comedy movies, I recommend  #{comedy_rec}"
-      else
-        puts "Since you don't like documentary, drama or comedy movies, I recommend the book The Silmarillion by J.R.R. Tolkien"
-      end
+  if (dram_ans >= 4) && (com_ans >= 4)
+    puts "Since you like drama and comedy movies, I recommend  #{dramedy_rec}"
+  elsif (dram_ans >= 4)
+    puts "Since you like drama movies, I recommend  #{drama_rec}"
+  elsif (com_ans >= 4)
+    puts "Since you like comedy movies, I recommend  #{comedy_rec}"
+  # stretch challenge
+  elsif (doc_ans <= 3) && (dram_ans <= 3) && (com_ans <= 3)
+    if (doc_ans > dram_ans) && (doc_ans > com_ans)
+      puts "Since you like documentaries, I recommend  #{documentary_rec}"
+    elsif (dram_ans > doc_ans) && (dram_ans > com_ans)
+      puts "Since you like drama movies, I recommend  #{drama_rec}"
+    elsif (com_ans > doc_ans) && (com_ans > dram_ans)
+      puts "Since you like comedy movies, I recommend  #{comedy_rec}"
+    end
+  # end of stretch challenge
+  else
+    puts "Since you don't like documentary, drama or comedy movies, I recommend the book The Silmarillion by J.R.R. Tolkien"
+  end
 end
