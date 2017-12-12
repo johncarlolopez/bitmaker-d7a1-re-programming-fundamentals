b6bf6e16413c00ab5f41a3cfee24f3d3a57ea803
John Carlo Lopez <condottiere.jc@gmail.com>
2017-12-12 14:08:16 -0500

    complete exercise 2

--- exercise2.rb
+++ exercise2.rb
@@ -13,16 +13,17 @@ puts "Rate your interest in comedy movies(1-5):"
 com_ans = gets.chomp.to_i
 
 
-
-if doc_ans == "yes"
+if doc_ans >= 4
   puts "Since you like documentaries, I recommend  #{documentary_rec}"
 end
-if ((dram_ans == "yes") && (com_ans == "yes"))
-  puts "Since you like drama and comedy movies, I recommend  #{dramedy_rec}"
-elsif (dram_ans == "yes")
-  puts "Since you like drama movies, I recommend  #{drama_rec}"
-elsif (com_ans == "yes")
-  puts "Since you like comedy movies, I recommend  #{comedy_rec}"
-elsif (doc_ans == "no")
-  puts "Since you don't like documentary, drama or comedy movies, I recommend the book The Silmarillion by J.R.R. Tolkien"
+if  (doc_ans <= 3)
+      if (dram_ans >= 4) && (com_ans >= 4)
+        puts "Since you like drama and comedy movies, I recommend  #{dramedy_rec}"
+      elsif (dram_ans >= 4)
+        puts "Since you like drama movies, I recommend  #{drama_rec}"
+      elsif (com_ans >= 4)
+        puts "Since you like comedy movies, I recommend  #{comedy_rec}"
+      else
+        puts "Since you don't like documentary, drama or comedy movies, I recommend the book The Silmarillion by J.R.R. Tolkien"
+      end
 end
